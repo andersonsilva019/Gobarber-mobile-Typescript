59c51b8bffbd4e61859206123019554d0d17e059
Anderson0017 <andeerson0017@gmail.com>
2020-08-27 11:50:31 -0300

    feat: input&Button

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Image } from 'react-native';
 import logoImg from '../../assets/logo.png';
+import { Input, Button } from '../../components';
+
 import { Container, Title } from './styles';
 
 const SignIn: React.FC = () => {
@@ -8,6 +10,10 @@ const SignIn: React.FC = () => {
     <Container>
       <Image source={logoImg} />
       <Title>Faça seu logon</Title>
+      <Input name="email" icon="mail" placeholder="E-mail" />
+      <Input name="password" icon="lock" placeholder="Senha" />
+
+      <Button onPress={() => {}}>Entrar</Button>
     </Container>
   );
 };
